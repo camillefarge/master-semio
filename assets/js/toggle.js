e0a00f1d9cf3132603b26075acfaae03e491dd82
camillefarge <camillefarge.pro@gmail.com>
2020-03-21 16:07:15 +0100

    page master1, master2, limoges

---
+++ assets/js/toggle.js
@@ -0,0 +1,8 @@
+$( "#toggle" ).click(function() {
+    // $(".filter").css("filter", "brightness(0.4)");
+    // $("body").css("overflow", "hidden");
+    $(".campus-2").toggle();
+    $(".campus").toggle();
+
+    // $(".limoges_localisation_title").css("color", "white").css("font-size", "80px");
+  });
